2ef13b63dbdce6754b7375b7840a8fa86ce1935d
Louise Carnevali <loucafumee@gmail.com>
2024-11-03 17:27:31 -0400

    updated next.config

--- pixelspring/next.config.mjs
+++ pixelspring/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["pixelspring-api.onrender.com"],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+};
 
 export default nextConfig;
